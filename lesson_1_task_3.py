51b9f41c4c39fd3112ebe01faf937eec0ab92a47
slavaV9001 <slava_9001_91@mail.ru>
2026-01-07 21:30:26 +0300

    Add files via upload

--- lesson_1_task_3.py
+++ lesson_1_task_3.py
@@ -1,3 +1,3 @@
-first_name = "Вячеслав"
-last_name = "Величко"
-print(f"Вас зовут:{last_name} {first_name}")
+first_name = input("Введите имя: ")
+last_name = input("Введите фамилию: ")
+print(f"Вас зовут: {last_name} {first_name}")
